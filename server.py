e05ee84adb18a2fef44fd2e51aac14d12593ca9d
Develthon <develthon@gmail.com>
2023-08-01 18:22:11 -0300

    Back end update

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    current_year = datetime.date.today().year
+    return render_template("index.html", year=current_year)
+
+@app.route('/sobre')
+def sobre():
+    current_year = datetime.date.today().year
+    return render_template("sobre.html", year=current_year)
+
+@app.route('/jogos')
+def jogos():
+    current_year = datetime.date.today().year
+    return render_template("jogos.html", year=current_year)
+
+@app.route('/contato')
+def contato():
+    current_year = datetime.date.today().year
+    return render_template("contato.html", year=current_year)
+
+if __name__ == "__main__":
+    app.run(debug=True)
